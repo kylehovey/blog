fbc5a7037fcc02de17b7ae4bf7e9a9a8ef961a6c
khoveyc@gmail.com <khoveyc@gmail.com>
2020-07-18 17:07:21 -0600

    Added table of animations for automata nebula

---
+++ images/automata_nebula/animations/similar/gol_like/montage.sh
@@ -0,0 +1,6 @@
+ convert \( 137480-B38-S238.gif -coalesce -append \) \
+          \( 236670-B123456-S123678.gif -coalesce -append \) \
+          \( 5640-B3-S013.gif -coalesce -append \) \
+          \( 5896-B38-S013.gif -coalesce -append \) \
+          \( 6152-B3-S23.gif -coalesce -append \) \
+          +append +repage -set delay 100 montage.gif
