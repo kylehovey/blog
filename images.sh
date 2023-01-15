362deb95cf46041738eb52cbe970c864449caded
kylehovey <khoveyc@gmail.com>
2023-01-14 18:41:58 -0800

    Added post on Vineyards and Harmony

---
+++ images.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cp "vineyard_loop 1.webp" /Users/speleo/Documents/Programs/Git/blog/images/vineyard/
+cp "1200px-Harmonic_partials_on_strings.svg.png" /Users/speleo/Documents/Programs/Git/blog/images/vineyard/
+cp "Pasted image 20230114135906.png" /Users/speleo/Documents/Programs/Git/blog/images/vineyard/
+cp "Pasted image 20230114140627.png" /Users/speleo/Documents/Programs/Git/blog/images/vineyard/
+cp "Pasted image 20230114140843.png" /Users/speleo/Documents/Programs/Git/blog/images/vineyard/
+cp "Pasted image 20230114142517.png" /Users/speleo/Documents/Programs/Git/blog/images/vineyard/
+cp "Pasted image 20230114142524.png" /Users/speleo/Documents/Programs/Git/blog/images/vineyard/
+cp "Pasted image 20230114143138.png" /Users/speleo/Documents/Programs/Git/blog/images/vineyard/
+cp "Pasted image 20230114160056.png" /Users/speleo/Documents/Programs/Git/blog/images/vineyard/
+cp "Pasted image 20230114162507.png" /Users/speleo/Documents/Programs/Git/blog/images/vineyard/
+cp "Pasted image 20230114180009.png" /Users/speleo/Documents/Programs/Git/blog/images/vineyard/
+cp "Pasted image 20230114164439.png" /Users/speleo/Documents/Programs/Git/blog/images/vineyard/
+cp "Pasted image 20230114170417.png" /Users/speleo/Documents/Programs/Git/blog/images/vineyard/
+cp "Pasted image 20230114170715.png" /Users/speleo/Documents/Programs/Git/blog/images/vineyard/
+cp "Pasted image 20230114171118.png" /Users/speleo/Documents/Programs/Git/blog/images/vineyard/
+cp "Pasted image 20230114171434.png" /Users/speleo/Documents/Programs/Git/blog/images/vineyard/
+cp "Pasted image 20230114175827.png" /Users/speleo/Documents/Programs/Git/blog/images/vineyard/
+cp "Pasted image 20230114172628.png" /Users/speleo/Documents/Programs/Git/blog/images/vineyard/
+cp "Pasted image 20230114174203.png" /Users/speleo/Documents/Programs/Git/blog/images/vineyard/
+cp "Pasted image 20230114174141.png" /Users/speleo/Documents/Programs/Git/blog/images/vineyard/
+cp "Pasted image 20230114174700.png" /Users/speleo/Documents/Programs/Git/blog/images/vineyard/
+cp "Pasted image 20230114174742.png" /Users/speleo/Documents/Programs/Git/blog/images/vineyard/
+cp "Pasted image 20230114175208.png" /Users/speleo/Documents/Programs/Git/blog/images/vineyard/
+cp "Pasted image 20220110180322.png" /Users/speleo/Documents/Programs/Git/blog/images/vineyard/
+cp "Pasted image 20220110183447.png" /Users/speleo/Documents/Programs/Git/blog/images/vineyard/
+cp "Pasted image 20220110183508.png" /Users/speleo/Documents/Programs/Git/blog/images/vineyard/
